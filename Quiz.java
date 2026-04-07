283765d0a312278f2892bb54a15b046d9c83f7d1
Paristarbell <nyoniparis37@gmail.com>
2026-04-08 01:30:58 +0200

    fixed some of my mistakes

--- Quiz.java
+++ Quiz.java
@@ -1,21 +1,8 @@
 import java.util.Scanner;
 public class Quiz{
     
-    // Answers
-    // Australia
-// 	Mars
-// Neil Armstrong
-// 	Avocado
-// 	Arendelle
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        // String question1="1.What is the only country in the world that is also a continent?";
-        // String question2="2.Which planet in our solar system is known as the 'Red Planet'?";
-        // String question3="3.Who was the first person to step foot on the moon?";
-        // String question4="4.What is the primary ingredient used to make traditional Guacamole?";
-        // String question5="5.What is the name of the fictional kingdom where the movie Frozen takes place?";
-        // String[] questions={question1,question2,question3,question4,question5};
-
         System.out.println("******************************************");
         System.out.println("WELCOME TO THE PARISTARBELL'S QUIZ GAME⭐");
         System.out.println("******************************************\n");
@@ -32,7 +19,7 @@ public class Quiz{
         String[] OptionsB={"A.Jupiter","B.Venus","C.Mars","D.Neptune"};
         String[] OptionsC={"A.Buzz Aldrin ","B.Sally Ride","C.Neil Armstrong","D.Guion Bluford"};
         String[] OptionsD={"A.Avocado ","B.Grapes","C.Banana","D.Mango"};
-        String[] OptionsE={"A.Beyonce","B.Arendelle","C.Jennifer Lawrence","D.Angelina Jolie"};
+        String[] OptionsE={"A.The pride lands","B.Arendelle","C.Fang","D.DunBroch"};
 
         String[][] options={OptionsA,OptionsB,OptionsC,OptionsD,OptionsE};
 
